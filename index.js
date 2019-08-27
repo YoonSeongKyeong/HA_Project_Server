66fde57ce88be2eeaf2c6b10484f3d475346be93
mdy6099 <mdy6099@naver.com>
2019-08-27 18:28:25 +0900

    made more structure for MVC pattern in DB

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 9999;
 const models = require("./models");
 const cors = require("cors")
+const bodyParser = require('body-parser')
 
 app.use(cors);
-
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 models.sequelize.sync().then(()=>
     app.listen(port, function () {
