b006589682fb0353664304b9316f6b6c6b41893b
mdy6099 <mdy6099@naver.com>
2019-08-27 18:21:03 +0900

    updated

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const models = require("./models");
 const cors = require("cors")
 
 app.use(cors);
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+
+
+
 models.sequelize.sync().then(()=>
     app.listen(port, function () {
         console.log(`server listening on port ${port}!`);
